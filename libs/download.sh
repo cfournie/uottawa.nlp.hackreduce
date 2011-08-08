0f924540ecb6aa51df4378dc9f8228970effe59c
cfournie <chris.m.fournier@gmail.com>
2011-08-08 17:22:36 -0400

    Added a download script to fetch and extract required libs.

---
+++ libs/download.sh
@@ -0,0 +1,4 @@
+wget http://ds60ft5bv5jal.cloudfront.net/latest/aws-java-sdk.zip
+unzip aws-java-sdk.zip
+wget http://nlp.stanford.edu/software/stanford-corenlp-v1.1.0.tgz
+tar -zxvf stanford-corenlp-v1.1.0.tgz
